c820aa9e476fa6ee6d3708b7e2ec441a20143973
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2021-10-18 21:50:32 -0500

    First Commit - Configure Project for Production

---
+++ AppLikeTwitter/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AppLikeTwitter
+//
+//  Created by José Francisco Castillo Rodríguez on 18/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
